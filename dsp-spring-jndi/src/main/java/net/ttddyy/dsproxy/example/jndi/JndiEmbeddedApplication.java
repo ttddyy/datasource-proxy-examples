f779611ee5ce863e9bd1c6890805c679863ac2c7
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2015-04-03 21:48:48 -0700

    sample spring-boot app that lookups JNDI using embedded tomcat

--- dsp-spring-jndi/src/main/java/net/ttddyy/dsproxy/example/jndi/JndiEmbeddedApplication.java
+++ dsp-spring-jndi/src/main/java/net/ttddyy/dsproxy/example/jndi/JndiEmbeddedApplication.java
@@ -1,6 +1,10 @@
 package net.ttddyy.dsproxy.example.jndi;
 
-import org.apache.catalina.Container;
+import net.ttddyy.dsproxy.QueryCount;
+import net.ttddyy.dsproxy.QueryCountHolder;
+import net.ttddyy.dsproxy.listener.CommonsLogLevel;
+import net.ttddyy.dsproxy.support.CommonsQueryCountLoggingFilter;
+import net.ttddyy.dsproxy.support.ProxyDataSource;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.tomcat.util.descriptor.web.ContextResource;
@@ -15,6 +19,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +32,7 @@ import java.util.Arrays;
  */
 @SpringBootApplication
 @RestController
-public class JNDIApplication {
+public class JndiEmbeddedApplication {
 
     /**
      * To initialize application when it is deployed to web container as a war file.
@@ -35,12 +40,12 @@ public class JNDIApplication {
     public static class ApplicationServletInitializer extends SpringBootServletInitializer {
         @Override
         protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-            return application.sources(JNDIApplication.class);
+            return application.sources(JndiEmbeddedApplication.class);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(JNDIApplication.class, args);
+        SpringApplication.run(JndiEmbeddedApplication.class, args);
     }
 
 
@@ -52,58 +57,79 @@ public class JNDIApplication {
                 tomcat.enableNaming();
                 TomcatEmbeddedServletContainer container = super.getTomcatEmbeddedServletContainer(tomcat);
 
-                for (Container child : container.getTomcat().getHost().findChildren()) {
-                    if (child instanceof Context) {
-                        ClassLoader contextClassLoader =
-                                ((Context) child).getLoader().getClassLoader();
-                        Thread.currentThread().setContextClassLoader(contextClassLoader);
-                        break;
-                    }
-                }
+                // JNDI lookup with embedded tomcat:  https://github.com/spring-projects/spring-boot/issues/2308
+                Context context = (Context) container.getTomcat().getHost().findChild("");
+                Thread.currentThread().setContextClassLoader(context.getLoader().getClassLoader());
                 return container;
             }
         };
+
+        // add JNDI datasource resources
         factory.addContextCustomizers((context) -> {
+
+            // actual datasource
             ContextResource resource = new ContextResource();
             resource.setName("jdbc/ds");
             resource.setType(DataSource.class.getName());
             resource.setProperty("driverClassName", "org.hsqldb.jdbc.JDBCDriver");
             resource.setProperty("url", "jdbc:hsqldb:mem:aname");
             context.getNamingResources().addResource(resource);
+
+            // ProxyDataSource
+            ContextResource dspResource = new ContextResource();
+            dspResource.setName("my/proxy");
+            dspResource.setType(ProxyDataSource.class.getName());
+            dspResource.setProperty("factory", "net.ttddyy.dsproxy.support.jndi.ProxyDataSourceObjectFactory");
+            dspResource.setProperty("dataSource", "java:comp/env/jdbc/ds");  // reference actual datasource
+            dspResource.setProperty("proxyName", "MyProxy");
+            dspResource.setProperty("listeners", "commons,count");
+            dspResource.setProperty("logLevel", "info");
+            context.getNamingResources().addResource(dspResource);
+
         });
 
         return factory;
     }
 
     @Bean
-    public JndiObjectFactoryBean jndiObjectFactoryBean() throws NamingException {
+    public DataSource jndiObjectFactoryBean() throws NamingException {
+        // look up JNDI for this application
         JndiObjectFactoryBean factory = new JndiObjectFactoryBean();
-        factory.setJndiName("java:comp/env/jdbc/ds");
-        factory.setExpectedType(DataSource.class);
-        factory.setLookupOnStartup(false);
-        return factory;
+        factory.setJndiName("java:comp/env/my/proxy");  // lookup ProxyDataSource
+        factory.setExpectedType(DataSource.class);  // as javax.sql.DataSource
+        factory.setLookupOnStartup(false);  // deffer the lookup because resources get registered at app startup
+        factory.afterPropertiesSet();
+        return (DataSource) factory.getObject();
     }
-
-    @Autowired
-    DataSource ds;
 
     @Bean
     public CommandLineRunner init() {
         return args -> {
-
-            // populate DB
+            // populate DB at app startup
             jdbcTemplate.execute("CREATE TABLE users(id INT, name VARCHAR(255))");
             jdbcTemplate.batchUpdate("INSERT INTO users (id, name) VALUES (?, ?)",
                     Arrays.asList(new Object[]{1, "foo"}, new Object[]{2, "bar"}));
         };
     }
 
+    @Bean
+    public CommonsQueryCountLoggingFilter commonsQueryCountLoggingFilter() {
+        return new CommonsQueryCountLoggingFilter(CommonsLogLevel.INFO);  // servlet filter
+    }
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @RequestMapping
-    public String index() throws Exception {
-        int count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
-        return "ABC" + count;
+    public ModelMap index() throws Exception {
+        int numOfUsers = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
+
+        QueryCount queryCount = QueryCountHolder.get("MyProxy");
+
+        ModelMap result = new ModelMap();
+        result.put("dataSourceName", "MyProxy");
+        result.put("queryCount", queryCount);
+        result.put("numOfUsers", numOfUsers);
+        return result;
     }
 }
