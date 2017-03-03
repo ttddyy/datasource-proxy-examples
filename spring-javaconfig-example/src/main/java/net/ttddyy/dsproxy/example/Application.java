bcf6b834ae37ffb92d473a99def04f5e076d4425
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2017-03-03 15:26:42 -0800

    Use datasource-proxy v1.4.1
    Also use hibernate query formatter and enable multiline output

--- spring-javaconfig-example/src/main/java/net/ttddyy/dsproxy/example/Application.java
+++ spring-javaconfig-example/src/main/java/net/ttddyy/dsproxy/example/Application.java
@@ -1,6 +1,10 @@
 package net.ttddyy.dsproxy.example;
 
+import net.ttddyy.dsproxy.listener.logging.DefaultQueryLogEntryCreator;
+import net.ttddyy.dsproxy.listener.logging.SystemOutQueryLoggingListener;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.hibernate.engine.jdbc.internal.FormatStyle;
+import org.hibernate.engine.jdbc.internal.Formatter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -32,27 +36,47 @@ public class Application {
         return databaseBuilder.setType(EmbeddedDatabaseType.HSQL).build();
     }
 
+    // use hibernate to format queries
+    private static class PrettyQueryEntryCreator extends DefaultQueryLogEntryCreator {
+        private Formatter formatter = FormatStyle.BASIC.getFormatter();
+
+        @Override
+        protected String formatQuery(String query) {
+            return this.formatter.format(query);
+        }
+    }
+
     @Bean
     @Primary
     public DataSource dataSource(DataSource actualDataSource) {
+        // use pretty formatted query with multiline enabled
+        PrettyQueryEntryCreator creator = new PrettyQueryEntryCreator();
+        creator.setMultiline(true);
+
+        SystemOutQueryLoggingListener listener = new SystemOutQueryLoggingListener();
+        listener.setQueryLogEntryCreator(creator);
+
         return ProxyDataSourceBuilder
                 .create(actualDataSource)
                 .name("MyDS")
-                .logQueryToSysOut()
+                .listener(listener)
                 .build();
     }
 
     @Bean
-    CommandLineRunner init(JdbcTemplate jdbcTemplate) {    // DataSourceAutoConfiguration creates jdbcTemplate
+    CommandLineRunner init(
+            JdbcTemplate jdbcTemplate) {    // DataSourceAutoConfiguration creates jdbcTemplate
         return args -> {
 
             System.out.println("**********************************************************");
 
             jdbcTemplate.execute("CREATE TABLE users (id INT, name VARCHAR(20))");
 
-            jdbcTemplate.batchUpdate("INSERT INTO users (id, name) VALUES (?, ?)", Arrays.asList(new Object[][]{{1, "foo"}, {2, "bar"}}));
+            jdbcTemplate.batchUpdate("INSERT INTO users (id, name) VALUES (?, ?)",
+                    Arrays.asList(new Object[][] { { 1, "foo" }, { 2, "bar" } }));
 
-            PreparedStatement preparedStatement = jdbcTemplate.getDataSource().getConnection().prepareStatement("INSERT INTO users (id, name) VALUES (?, ?)");
+            PreparedStatement preparedStatement = jdbcTemplate.getDataSource().getConnection()
+                    .prepareStatement("INSERT INTO users (id, name) VALUES (?, ?)");
             preparedStatement.setString(2, "FOO");
             preparedStatement.setInt(1, 3);
             preparedStatement.addBatch();
