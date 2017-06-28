bfc86e9510f390a961720c8f2946329766e14bb8
rajadilipkolli <rajadilipkolli@gmail.com>
2017-06-28 13:26:34 +0530

    adds spring batching

--- springboot-autoconfig-example/src/main/java/net/ttddyy/dsproxy/example/User.java
+++ springboot-autoconfig-example/src/main/java/net/ttddyy/dsproxy/example/User.java
@@ -2,12 +2,13 @@ package net.ttddyy.dsproxy.example;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
     
     private String name;
