82c0d7b18b7c88b8834b0a7f84bf2f30d80477cf
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2017-08-14 04:57:36 -0700

    Add more documentation
    Add links to external posts for why BeanPostProcessor needs to return
    a proxy object.
    
    Issue: #10

--- springboot-autoconfig-example/src/main/java/net/ttddyy/dsproxy/example/DatasourceProxyBeanPostProcessor.java
+++ springboot-autoconfig-example/src/main/java/net/ttddyy/dsproxy/example/DatasourceProxyBeanPostProcessor.java
@@ -1,8 +1,7 @@
 package net.ttddyy.dsproxy.example;
 
-import java.lang.reflect.Method;
-import javax.sql.DataSource;
-
+import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
+import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.aop.framework.ProxyFactory;
@@ -10,8 +9,8 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
-import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
-import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import javax.sql.DataSource;
+import java.lang.reflect.Method;
 
 @Component
 public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
@@ -19,8 +18,12 @@ public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) {
         if (bean instanceof DataSource) {
-            // see this blog post for config with spring-boot 2.0.0.M3:
-            // http://blog.arnoldgalovics.com/2017/06/26/configuring-a-datasource-proxy-in-spring-boot/
+            // Instead of directly returning a less specific datasource bean
+            // (e.g.: HikariDataSource -> DataSource), return a proxy object.
+            // See following links for why:
+            //   https://stackoverflow.com/questions/44237787/how-to-use-user-defined-database-proxy-in-datajpatest
+            //   https://gitter.im/spring-projects/spring-boot?at=5983602d2723db8d5e70a904
+            //   http://blog.arnoldgalovics.com/2017/06/26/configuring-a-datasource-proxy-in-spring-boot/
             final ProxyFactory factory = new ProxyFactory(bean);
             factory.setProxyTargetClass(true);
             factory.addAdvice(new ProxyDataSourceInterceptor((DataSource) bean));
@@ -39,16 +42,19 @@ public class DatasourceProxyBeanPostProcessor implements BeanPostProcessor {
 
         public ProxyDataSourceInterceptor(final DataSource dataSource) {
             super();
-            this.dataSource = ProxyDataSourceBuilder.create(dataSource).name("MyDS").multiline()
-                    .logQueryBySlf4j(SLF4JLogLevel.INFO).build();
+            this.dataSource = ProxyDataSourceBuilder.create(dataSource)
+                    .name("MyDS")
+                    .multiline()
+                    .logQueryBySlf4j(SLF4JLogLevel.INFO)
+                    .build();
         }
 
         @Override
         public Object invoke(final MethodInvocation invocation) throws Throwable {
-            final Method proxyMethod = ReflectionUtils.findMethod(dataSource.getClass(),
+            final Method proxyMethod = ReflectionUtils.findMethod(this.dataSource.getClass(),
                     invocation.getMethod().getName());
             if (proxyMethod != null) {
-                return proxyMethod.invoke(dataSource, invocation.getArguments());
+                return proxyMethod.invoke(this.dataSource, invocation.getArguments());
             }
             return invocation.proceed();
         }
