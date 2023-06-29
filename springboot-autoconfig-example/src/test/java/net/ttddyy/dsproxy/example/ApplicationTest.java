88ec0534fcc141fd0dd038831c46d922f20540a5
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-29 11:46:11 +0000

    Upgrade to spring boot 3.1.1

--- springboot-autoconfig-example/src/test/java/net/ttddyy/dsproxy/example/ApplicationTest.java
+++ springboot-autoconfig-example/src/test/java/net/ttddyy/dsproxy/example/ApplicationTest.java
@@ -5,21 +5,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+class ApplicationTest {
 
     @Autowired
     private UserRepository userRepository;
-    
+
     @Test
-    public void test() {
+    void test() {
        User user = new User();
        user.setName("foo");
        User user1 = new User();
